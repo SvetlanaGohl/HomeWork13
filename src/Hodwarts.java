0577cd779ad3431bc7f5b21934949494f4f3ba4f
Svetlana Gohl <sm181183@mail.ru>
2023-07-27 01:27:17 +0300

    Начальный коммит

---
+++ src/Hodwarts.java
@@ -0,0 +1,15 @@
+public class Hodwarts {
+    private String name;
+    private String surname;
+    private int powerWitchcraft;
+    private int transgression;
+
+    public Hodwarts(String name, String surname, int powerWitchcraft, int transgression) {
+        this.name = name;
+        this.surname = surname;
+        this.powerWitchcraft = powerWitchcraft;
+        this.transgression = transgression;
+    }
+}
+
+
